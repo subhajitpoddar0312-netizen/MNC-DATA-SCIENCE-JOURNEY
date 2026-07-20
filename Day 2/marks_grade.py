53c001acff44656adc24d8d9e4c988671b527d36
subhajitpoddar0312-netizen <subhajitpoddar0312@gmail.com>
2026-07-20 21:46:30 +0530

    Add grading logic based on marks

---
+++ Day 2/marks_grade.py
@@ -0,0 +1,13 @@
+marks=75
+if marks>=90:
+  print("A+")
+elif marks>=80:
+  print("A")
+elif marks>=70:
+  print("B")
+elif marks>=60:
+  print("C")
+elif marks>=33:
+  print("pass")
+else:
+  print("fail")
